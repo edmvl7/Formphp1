6fce578bf28a06bf006ecbf65849163fc0ae8236
edmvl7 <[valerie-edmond_student2023@wilder.school]>
2023-02-24 19:26:45 +0100

    Add formphp2

--- form.php
+++ form.php
@@ -1,3 +1,29 @@
+<?php
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if(!isset($_POST['user_name']) || ($_POST['user_name']) === '')
+            $error[] = "The first name is required";
+        if(!isset($_POST['user_lname']) || trim($_POST['user_lname']) === '')
+            $error[] = "The last name is required";
+        if(!isset($_POST['user_email']) || trim($_POST['user_email']) || var_dump(filter_var('bob@example.com', FILTER_VALIDATE_EMAIL)) === '')
+            $error[] = "The email is required";
+        if(!isset($_POST['user_number']) || trim($_POST['user_email']) === '')
+            $error[] = "The number is required";
+        if(!isset($_POST['user_subject']) || trim($_POST['user_email']) === '')
+            $error[] = "The subject is required";
+        if(!isset($_POST['user_message']) || trim($_POST['user_email']) === '')
+            $error[] = "The message is required";
+
+            if(empty($errors)) {
+                header('Location: thanks.php');
+            }
+
+
+    }
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,27 +40,27 @@
             <div>
                 <label for="fname">First name : </label>
                 <br>
-                <input type="text" id="fname" name="user_name"> 
+                <input type="text" id="fname" name="user_name" required> 
             </div>
             <div>
                 <label for="lname">Last name : </label>
                 <br>
-                <input type="text" id="lname" name="user_lname"> 
+                <input type="text" id="lname" name="user_lname" required> 
             </div>
             <div>
                 <label for="mail">Email : </label>
                 <br>
-                <input type="email" id="mail" name="user_email"> 
+                <input type="email" id="mail" name="user_email" required> 
             </div>
             <div>
                 <label for="number">Phone number : </label>
                 <br>
-                <input type="number" id="number" name="user_number"> 
+                <input type="number" id="number" name="user_number" required> 
             </div>
             <div>
             <label for="subject">Subject : </label>
             <br>
-            <select name="subject" id="user_subject">
+            <select name="subject" id="user_subject" required>
                 <option value="information">Information</option>
                 <option value="Review">Review</option>
                 <option value="rendezVous">Rendez-vous</option>
@@ -45,7 +71,7 @@
             <div>
                 <label for="message">Message: </label>
                 <br>
-                <textarea type="text" id="messege" name="user_message"> </textarea>
+                <textarea type="text" id="messege" name="user_message" required> </textarea>
             </div>
             <div>
                 <button type="submit">VALIDATION</button>
